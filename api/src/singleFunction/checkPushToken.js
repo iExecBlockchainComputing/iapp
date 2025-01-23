f2060bdf4fcdfd90acb7d41b8ecd6f1b5b2d661f
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-01-23 13:42:36 +0100

    check push token + reorder checks to throw early

---
+++ api/src/singleFunction/checkPushToken.js
@@ -0,0 +1,43 @@
+import { jwtDecode } from 'jwt-decode';
+
+/**
+ * Checks the pushToken can be used to push on a specified repository
+ */
+export async function checkPushToken({ pushToken, repository }) {
+  const decoded = jwtDecode(pushToken);
+
+  if (!Array.isArray(decoded?.access)) {
+    throw Error('Missing access in jwt');
+  }
+  // check claims
+  let hasPushAccess = false;
+  decoded?.access?.forEach((access) => {
+    if (
+      // repo
+      access?.type === 'repository' &&
+      access?.name === repository &&
+      // push action
+      Array.isArray(access?.actions) &&
+      access?.actions?.includes('pull') &&
+      access?.actions?.includes('push')
+    ) {
+      hasPushAccess = true;
+    }
+  });
+  if (!hasPushAccess) {
+    throw Error(`Missing push access on ${repository}`);
+  }
+  // check validity against server
+  const DOCKER_REGISTRY = 'registry-1.docker.io';
+  const response = await fetch(`https://${DOCKER_REGISTRY}/v2/`, {
+    method: 'HEAD',
+    headers: {
+      Authorization: `Bearer ${pushToken}`,
+    },
+  }).catch((e) => {
+    throw Error(`Failed to contact server ${DOCKER_REGISTRY}`, { cause: e });
+  });
+  if (!response.status === 200) {
+    throw Error(`Token validity check against ${DOCKER_REGISTRY} failed`);
+  }
+}
