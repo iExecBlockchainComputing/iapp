49635bbe609dcbffca1a29c9de4ffd0963c089a3
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-06-26 13:20:58 +0200

    fix: disable huge images pull

--- cli/src/execDocker/docker.ts
+++ cli/src/execDocker/docker.ts
@@ -45,7 +45,7 @@ export async function dockerBuild({
   const buildImageStream = await docker.buildImage(buildArgs, {
     t: tag,
     platform,
-    pull: true, // docker store does not support multi platform image, this can cause issues when switching build target platform, pulling ensures the right image is used
+    // pull: true, // docker store does not support multi platform image, this can cause issues when switching build target platform, pulling ensures the right image is used
     abortSignal: signal,
   });
 
