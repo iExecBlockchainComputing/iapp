346000ea1e8c3e7f444df5563f16eab3bcf06912
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-07-15 12:30:34 +0200

    feat: check required RLC balance for iapp run

--- cli/src/cli-helpers/ensureBalances.ts
+++ cli/src/cli-helpers/ensureBalances.ts
@@ -1,37 +1,69 @@
 import { warnBox } from './box.js';
 import { emphasis, command } from './color.js';
 import type { Spinner } from './spinner.js';
-import { IExec } from 'iexec';
+import { BN, IExec, utils } from 'iexec';
 
 export async function ensureBalances({
   spinner,
   iexec,
+  nRlcMin,
+  weiMin,
 }: {
   spinner: Spinner;
   iexec: IExec;
-}): Promise<void> {
+  nRlcMin?: BN;
+  weiMin?: BN;
+}): Promise<{
+  wei: BN;
+  nRLC: BN;
+  stake: BN;
+}> {
   const chainId = await iexec.config.resolveChainId();
-  if (chainId === 134) {
-    return;
-  }
   const address = await iexec.wallet.getAddress();
   const [{ nRLC, wei }, { stake }] = await Promise.all([
     iexec.wallet.checkBalances(address),
     iexec.account.checkBalance(address),
     // TODO check voucher?
   ]);
-  const hasNative = !wei.isZero();
-  const hasRlc = !stake.isZero || !nRLC.isZero();
-  if (hasNative && hasRlc) {
-    return;
+
+  const totalRlc = stake.add(nRLC);
+
+  const missingNative =
+    (chainId !== 134 && wei.isZero()) || (!!weiMin && wei.lt(weiMin));
+  const missingRlc =
+    (chainId !== 134 && totalRlc.isZero()) ||
+    (!!nRlcMin && totalRlc.lt(nRlcMin));
+
+  if (!missingNative && !missingRlc) {
+    return {
+      wei,
+      nRLC,
+      stake,
+    };
   }
 
   const helpers = [];
-  if (!hasNative) helpers.push(' - Native asset balance is empty');
-  if (!hasRlc) helpers.push(' - RLC balance is empty');
+  if (missingNative) {
+    const msg = wei.isZero()
+      ? ' - Native balance is empty'
+      : ' - Native balance is insufficient';
+    const requirement = weiMin
+      ? ` (requires ${utils.formatEth(weiMin as BN)} ether)`
+      : '';
+    helpers.push(`${msg}${requirement}`);
+  }
+  if (missingRlc) {
+    const msg = totalRlc.isZero()
+      ? ' - RLC balance is empty'
+      : ' - RLC balance is insufficient';
+    const requirement = nRlcMin
+      ? ` (requires ${utils.formatRLC(nRlcMin as BN)} RLC)`
+      : '';
+    helpers.push(`${msg}${requirement}`);
+  }
 
   spinner.log(
-    warnBox(`Current chain requires native asset to pay transactions fees and RLC to pay iApp runs!
+    warnBox(`Current chain requires ${chainId !== 134 ? 'native asset to pay transactions fees and ' : ''}RLC to pay iApp runs!
  
 Your wallet balance is insufficient:
 ${helpers.join('\n')}
@@ -39,7 +71,8 @@ ${helpers.join('\n')}
 You can either:
  - Fund your wallet ${emphasis(address)}
  - Import another wallet (run ${command('iapp wallet import')})
- - Select an imported wallet (run ${command('iapp wallet select')})`)
+ - Select an imported wallet (run ${command('iapp wallet select')})
+ - Use another chain (use option ${command('--chain <name>')})`)
   );
   throw Error(`Balance is insufficient`);
 }
