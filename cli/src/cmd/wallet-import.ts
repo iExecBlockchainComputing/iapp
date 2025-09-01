6d736d0a665645d66c3875f25633f60895251710
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-09-01 17:34:33 +0200

    fix: resolve bad config file detection in wallet commands

--- cli/src/cmd/wallet-import.ts
+++ cli/src/cmd/wallet-import.ts
@@ -3,10 +3,12 @@ import { handleCliError } from '../cli-helpers/handleCliError.js';
 import { getSpinner } from '../cli-helpers/spinner.js';
 import * as color from '../cli-helpers/color.js';
 import { warnBeforeDeletePrivateKey } from '../cli-helpers/warnBeforeDeletePrivateKey.js';
+import { goToProjectRoot } from '../cli-helpers/goToProjectRoot.js';
 
 export async function walletImport() {
   const spinner = getSpinner();
   try {
+    await goToProjectRoot({ spinner });
     spinner.text = 'Importing wallet';
     await warnBeforeDeletePrivateKey({ spinner });
     const signer = await askForImportWallet({ spinner });
--- cli/src/cmd/wallet-select.ts
+++ cli/src/cmd/wallet-select.ts
@@ -4,10 +4,12 @@ import * as color from '../cli-helpers/color.js';
 import { KEYSTORE_PATH, listWalletsFromKeystore } from '../utils/keystore.js';
 import { warnBeforeDeletePrivateKey } from '../cli-helpers/warnBeforeDeletePrivateKey.js';
 import { readIAppConfig, writeIAppConfig } from '../utils/iAppConfigFile.js';
+import { goToProjectRoot } from '../cli-helpers/goToProjectRoot.js';
 
 export async function walletSelect() {
   const spinner = getSpinner();
   try {
+    await goToProjectRoot({ spinner });
     spinner.text = 'Selecting wallet';
     await warnBeforeDeletePrivateKey({ spinner });
     const config = await readIAppConfig();
