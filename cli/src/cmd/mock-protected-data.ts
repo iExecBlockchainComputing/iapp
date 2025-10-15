80d79a29d437618ec4a67d22e16ca5a1987ebff3
paypes <43441600+abbesBenayache@users.noreply.github.com>
2025-10-15 13:56:04 +0200

    feat: improve CLI messaging for mock protected data

--- cli/src/cmd/mock-protected-data.ts
+++ cli/src/cmd/mock-protected-data.ts
@@ -27,7 +27,7 @@ export async function mockProtectedData() {
         type: 'text',
         name: 'key',
         message:
-          'Protected Data can have multiple asset types (email, passport, id etc..). Which one do you want to pin first?',
+          'Protected Data is an object holding different asset types (email, passport, ID, etc.). Define a key to access it?',
       });
 
       // check key is valid
