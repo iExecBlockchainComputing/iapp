a836e4cff321085792b264a75db6d7697bad5363
pjt <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-12-01 09:40:10 +0100

    fix!: drop support for deprecated APIs (#254)
    * fix!: drop support for deprecated APIs
    
    BREAKING CHANGE: `POST /sconify` is dropped, websocket API request `SCONIFY_BUILD` must be used instead; `POST /sconify/build` is dropped, websocket API request `SCONIFY_BUILD` must be used instead; websocket API request `SCONIFY` is dropped, websocket API request `SCONIFY_BUILD` must be used instead; template `Python` is dropped, template `Python3.13` must be used instead; sconeVersion `v5` or `undefined` is dropped, sconeVersion `v5.9` must be used instead; sconeProd `false` or `undefined` is dropped, sconeProd `true` must be used instead.
    
    * chore: clean iexec dependency

--- api/src/constants/constants.ts
+++ api/src/constants/constants.ts
@@ -6,10 +6,11 @@ export const SCONIFY_IMAGE_NAME = `registry.scontain.com/scone-production/iexec-
  *
  * NB: "v5" is the legacy scone version v5.7 (name constrained by version parsing in workerpool)
  */
-export type SconeVersion = 'v5' | 'v5.9';
+export type SconeVersion = 'v5.9';
+
+export type OutdatedSconeVersion = 'v5';
 
 export const SCONIFY_IMAGE_VERSIONS: Record<SconeVersion, string> = {
-  v5: '5.7.6-v15',
   'v5.9': '5.9.1-v16',
 };
 
@@ -17,7 +18,9 @@ export const SCONIFY_IMAGE_VERSIONS: Record<SconeVersion, string> = {
 // It will be pulled if it's not yet in the local docker
 // https://hub.docker.com/layers/library/alpine/3.15/images/sha256-6a0657acfef760bd9e293361c9b558e98e7d740ed0dffca823d17098a4ffddf5?context=explore
 
-export type TemplateName = 'JavaScript' | 'Python' | 'Python3.13';
+export type TemplateName = 'JavaScript' | 'Python3.13';
+
+export type OutdatedTemplateName = 'Python';
 
 export const TEMPLATE_CONFIG: Record<
   TemplateName,
@@ -29,16 +32,9 @@ export const TEMPLATE_CONFIG: Record<
   JavaScript: {
     // node binary name does not change from one version to another
     binary: '/usr/local/bin/node',
-    // deprecated for scone 5.7 this was necessary
-    sconeCuratedImage:
-      'registry.scontain.com:5050/sconecuratedimages/node:14.4.0-alpine3.11',
   },
   'Python3.13': {
     binary: '/usr/local/bin/python3.13',
-  },
-  // deprecated legacy template name Python used Python 3.8
-  Python: {
-    binary: '/usr/local/bin/python3.8',
   },
 };
 
